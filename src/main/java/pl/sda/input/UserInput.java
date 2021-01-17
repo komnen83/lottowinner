fc6a8c470fcedff42c7d0808813630609ed245eb
komnen <kuba.bauman@gmail.com>
2021-01-17 22:58:11 +0100

    add Tests

--- src/main/java/pl/sda/input/UserInput.java
+++ src/main/java/pl/sda/input/UserInput.java
@@ -4,7 +4,6 @@ import pl.sda.model.GameType;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class UserInput implements Input {
 
@@ -19,33 +18,30 @@ public class UserInput implements Input {
     @Override
     public InputResult getInputResults() {
         GameType gameType = gameTypeProvider.getGameType();
-        List<Integer> userNumbers = new ArrayList<>();
+        NumberValidator numberValidator = new NumberValidator(gameType);
+        List<Integer> myNumbers = new ArrayList<>();
 
         do {
-            System.out.print("podaj liczbe nr " + (userNumbers.size() + 1) + ": ");
+            System.out.print("podaj liczbe nr " + (myNumbers.size() + 1) + ": ");
 
             String numAsString = scanner.next();
             int newNumber = 0;
 
-            try {
-                newNumber = Integer.parseInt(numAsString);
-            } catch (NumberFormatException e) {
-                System.out.println("Musisz podac liczbe");
+            newNumber = numberValidator.isItANumber(newNumber, numAsString);
+
+            if (myNumbers.contains(newNumber)) {
+                System.out.println("taka licza juz zostala wprowadzona");
                 continue;
             }
 
-            if (userNumbers.contains(newNumber)) {
-                System.out.println("Już podałeś taką liczbę, podaj inną");
+            if (numberValidator.isOutOfRange(newNumber)) {
+                System.out.println("liczba jest z poza zakresu");
                 continue;
             }
 
-            if (newNumber > 0 && newNumber <= gameType.getRange()) {
-                userNumbers.add(newNumber);
-            } else {
-                System.out.println("liczba: " + newNumber + " jest z poza zakresu 1-" + gameType.getRange());
-            }
-        } while (userNumbers.size() < gameType.getMaxNumberCount());
+            myNumbers.add(newNumber);
+        } while (myNumbers.size() < gameType.getMaxNumberCount());
 
-        return new InputResult(gameType, userNumbers);
+        return new InputResult(gameType, myNumbers);
     }
 }
